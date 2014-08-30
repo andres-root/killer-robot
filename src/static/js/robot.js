34aa8d616efdc2d840fa3a4f088c5cd9d5b8553b
AndresRoot <strike1609@gmail.com>
2014-08-29 22:23:42 -0500

    init repo

--- src/static/js/robot.js
+++ src/static/js/robot.js
@@ -2,25 +2,25 @@ var canvas = null;
 var context = null;
 var frameRate = 1000/30;
 var frame 	= 0;
-var assets = ['static/img/robowalk00.png',
-              'static/img/robowalk01.png',
-              'static/img/robowalk02.png',
-              'static/img/robowalk03.png',
-              'static/img/robowalk04.png',
-              'static/img/robowalk05.png',
-              'static/img/robowalk06.png',
-              'static/img/robowalk07.png',
-              'static/img/robowalk08.png',
-              'static/img/robowalk09.png',
-              'static/img/robowalk10.png',
-              'static/img/robowalk11.png',
-              'static/img/robowalk12.png',
-              'static/img/robowalk13.png',
-              'static/img/robowalk14.png',
-              'static/img/robowalk15.png',
-              'static/img/robowalk16.png',
-              'static/img/robowalk17.png',
-              'static/img/robowalk18.png'
+var assets = ['src/static/img/robowalk00.png',
+              'src/static/img/robowalk01.png',
+              'src/static/img/robowalk02.png',
+              'src/static/img/robowalk03.png',
+              'src/static/img/robowalk04.png',
+              'src/static/img/robowalk05.png',
+              'src/static/img/robowalk06.png',
+              'src/static/img/robowalk07.png',
+              'src/static/img/robowalk08.png',
+              'src/static/img/robowalk09.png',
+              'src/static/img/robowalk10.png',
+              'src/static/img/robowalk11.png',
+              'src/static/img/robowalk12.png',
+              'src/static/img/robowalk13.png',
+              'src/static/img/robowalk14.png',
+              'src/static/img/robowalk15.png',
+              'src/static/img/robowalk16.png',
+              'src/static/img/robowalk17.png',
+              'src/static/img/robowalk18.png'
              ];
 var frames = [];  
 
